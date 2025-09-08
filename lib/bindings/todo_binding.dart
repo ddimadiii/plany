a9d4380aca28f695b87aeba015e4a790f8609503
ddimadiii <ddimadiii.com>
2025-09-09 00:39:46 +0700

    fix/bindings

---
+++ lib/bindings/todo_binding.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import 'package:plany/controllers/auth_controller.dart';
+import 'package:plany/controllers/bottomnav_controller.dart';
+import 'package:plany/controllers/todo_controller.dart';
+
+class TodoBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<AuthController>(() => AuthController());
+    Get.lazyPut<BottomnavController>(() => BottomnavController());
+    Get.lazyPut<TodoController>(() => TodoController());
+  }
+}
