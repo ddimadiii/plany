b36111007f11aed652c7aa23c38804a54f9d166c
ddimadiii <ddimadiii.com>
2025-09-08 22:41:42 +0700

    fix/auth_controller

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,3 +1,36 @@
-class AuthController {
-  
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AuthController extends GetxController {
+  final username = TextEditingController();
+  final password = TextEditingController();
+  var obscurePassword = true.obs;
+
+  void auth() {
+    String usernameToString = username.text.trim();
+    String passwordToString = password.text.trim();
+
+    if (usernameToString == "madi" && passwordToString == "keiko") {
+      Get.snackbar(
+        "Auth",
+        "Login Berhasil",
+        backgroundColor: const Color.fromARGB(200, 17, 148, 208),
+        colorText: Colors.white,
+        snackPosition: SnackPosition.TOP,
+        duration: const Duration(seconds: 2),
+      );
+      Get.toNamed('/initial');
+    } else {
+      Get.snackbar(
+        "Auth",
+        "Login Gagal",
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        snackPosition: SnackPosition.BOTTOM,
+        duration: const Duration(seconds: 2),
+      );
+    }
+    username.clear();
+    password.clear();
+  }
 }
