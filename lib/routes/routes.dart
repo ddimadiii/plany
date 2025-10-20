e54c30688450efd24cca40e5ad48df99a71c6e82
ddimadiii <ddimadiii.com>
2025-10-21 05:22:49 +0700

    remove unused import

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,7 +1,5 @@
 import 'package:get/get.dart';
 import 'package:plany/bindings/bindings.dart';
-import 'package:plany/bindings/splash_binding.dart';
-import '../bindings/todo_binding.dart';
 import '../pages/add_todo_page.dart';
 import '../pages/history_page.dart';
 import '../pages/home_page.dart';
